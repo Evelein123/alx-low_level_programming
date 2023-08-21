7f936951a09f3b421ca23a276670bb26ad36eba4
Evelein123 <youssryeveleen@gmail.com>
2023-08-21 07:45:00 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,15 @@
 
 /**
  * print_rev - prints reversed string, followed by a new line
- *
  * @s: pointer to the string to print
- *
  * Return void
  */
 
 void print_rev(char *s)
 {
-	int i - 0;
+	int i = 0;
 
-	while (s[i])
+	while (s[i] != '\0')
 		i++;
 
 	while (i--)
