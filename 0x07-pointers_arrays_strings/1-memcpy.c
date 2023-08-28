c81ec28a2d90f799116882a0d7c54b5b2475a3a7
Evelein123 <youssryeveleen@gmail.com>
2023-08-28 03:51:40 -0700

    betty 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	/* loop through the source memory area and copy each byte to destination */
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	/* return the pointer to the destination memory area */
+	return (dest);
+}
