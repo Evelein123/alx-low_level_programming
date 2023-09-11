cf02fdc08a3f1b180ebd7400cf614fe3fe06e589
Evelein123 <youssryeveleen@gmail.com>
2023-09-11 07:45:01 -0700

    gfhbjf

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_H /* check if the header file is already included */
+#define OBJECT_LIKE_MACRO_H /* define the header file name */
+
+#define SIZE 1024 /* define the macro name and value */
+
+#endif /* end of the header file */
