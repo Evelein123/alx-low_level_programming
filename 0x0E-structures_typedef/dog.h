faa22ca6d5482a7a8f03aab08a04b45e871639f1
Evelein123 <youssryeveleen@gmail.com>
2023-09-11 04:53:36 -0700

    ghjadc

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/* File: dog.h */
+
+/**
+ * struct dog - a new type struct dog with the following elements:
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
