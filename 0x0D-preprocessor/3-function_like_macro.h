5c731f22738a0b5c18d38cec05200871591fb321
Evelein123 <youssryeveleen@gmail.com>
2023-09-12 05:18:20 -0700

    fds

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+/*This macro computes the absolute value of a number x*/
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
