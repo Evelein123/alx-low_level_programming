65b20e112f6fea7b1b0fabf5cc4996444658d5b5
Evelein123 <youssryeveleen@gmail.com>
2023-09-12 05:14:09 -0700

    vdvd

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,11 @@
+/* This program prints the name of the file it was compiled from*/
 #include <stdio.h>
-
+/*The main function of the program*/
+/*It takes no arguments and returns 0 on success*/
 int main(void)
 {
+	/*Use the predefined macro __FILE__ to get the source file name*/
 	printf("%s\n", __FILE__);
+	/*Return 0 to indicate success*/
 	return (0);
 }
