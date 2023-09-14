ea4aa298aabbe3d5a3c144cf9441d7e7cf8c4ee2
Evelein123 <youssryeveleen@gmail.com>
2023-09-14 13:50:43 -0700

    fvs

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void _puts(char *str);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
